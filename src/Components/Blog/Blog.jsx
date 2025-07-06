c111def0c086d14cb4c0eba5854a6c8e3ca83cd3
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-07-06 22:55:33 +0600

    more update UI like, show author name, image, hashtag

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,11 +3,30 @@ import './Blog.css';
 
 const Blog = ({blog}) => {
     
-    const {title} = blog;
+    const {title,cover,author,author_img, reading_time, posted_date, hashtags} = blog;
 
   return (
     <div>
-        <h1>Title : {title}</h1>
+        <img src={cover} alt='' />
+        <div className='flex justify-between'>
+
+            <div className='flex my-5'> 
+                <img className='w-14' src={author_img} alt="" />
+                <div className='ml-6'>
+                    <h3 className='text-2xl'>{author}</h3>
+                    <p>{posted_date}</p>
+                </div>
+            </div>
+            <div>
+                <span>{reading_time} min read</span>
+            </div>
+        </div>
+        <h1 className='text-4xl'>Title : {title}</h1>
+        <p>
+            {
+                hashtags.map((hash, idx) => <span key={idx}><a href=""> #{hash}</a></span>)
+            }
+        </p>
     </div>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 import profile from '../../assets/images/profile.png'
 const Header = () => {
   return (
-    <div className='flex items-center justify-between p-4 mx-4 border-b-2'>
+    <div className='flex items-center justify-between border-b-2'>
         <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
         <img src={profile} alt="" />
     </div>
