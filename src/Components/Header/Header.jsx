475754b373c369cc1aadc6d80cc85aad333d4cc1
rakib2299 <mdrakib@combosoft.co.uk>
2025-07-11 12:11:51 +0600

    style in header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 import profile from '../../assets/images/profile.png'
 const Header = () => {
   return (
-    <div className='flex items-center justify-between border-b-2'>
+    <div className='flex items-center justify-between pb-4 border-b-2'>
         <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
         <img src={profile} alt="" />
     </div>
